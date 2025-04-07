1276c014078d0f87742abbaf966e12ec38a65813
Peter Polidoro <peter@polidoro.io>
2025-04-07 13:19:32 -0400

    Example works!

--- examples/TestMongoose/TestMongoose.ino
+++ examples/TestMongoose/TestMongoose.ino
@@ -11,7 +11,16 @@ struct mg_tcpip_spi spi = {
     [](void *, uint8_t c) { return SPI.transfer(c); }, // Execute transaction
 };
 struct mg_mgr mgr;                                     // Mongoose event manager
-struct mg_tcpip_if mif = {.mac = {2, 0, 1, 2, 3, 5}};  // Network interface
+struct mg_tcpip_if mif; // Network interface
+
+// // Construct MAC address from the unique board ID
+#include "pico/unique_id.h"
+static inline void genmac(unsigned char *mac) {
+  pico_unique_board_id_t board_id;
+  pico_get_unique_board_id(&board_id);
+  mac[0] = 2;
+  memcpy(&mac[1], &board_id.id[3], 5);
+}
 
 // Used by Mongoose for time tracking
 uint64_t mg_millis(void) {
@@ -99,6 +108,11 @@ void setup() {
   mg_mgr_init(&mgr);        // Initialise Mongoose event manager
   mg_log_set(MG_LL_DEBUG);  // Set debug log level
   mg_log_set_fn([](char ch, void *) { Serial.print(ch); }, NULL);  // Log serial
+  genmac(mif.mac);
+  // mif.enable_dhcp_client = false;
+  // mif.ip = MG_IPV4(192, 168, 10, 55);
+  // mif.gw = MG_IPV4(192, 168, 10, 1);
+  // mif.mask = MG_IPV4(255, 255, 255, 0);
   mif.driver = &mg_tcpip_driver_w5500;  // Use W5500 built-in driver
   mif.driver_data = &spi;               // Pass SPI interface to W5500 driver
   mg_tcpip_init(&mgr, &mif);            // Initialise built-in TCP/IP stack
@@ -111,63 +125,3 @@ void setup() {
 void loop() {
   mg_mgr_poll(&mgr, 1);
 }
-// #define SS_PIN 17            // Slave select pin
-// #define LED_PIN LED_BUILTIN  // LED pin
-
-// SPISettings ethernet_spi_settings(4000000,
-//   MSBFIRST,
-//   SPI_MODE0);
-
-// struct mg_tcpip_spi spi = {
-//     NULL,  // SPI metadata
-//     [](void *) { digitalWrite(SS_PIN, LOW); SPI.beginTransaction(ethernet_spi_settings); },
-//     [](void *) { digitalWrite(SS_PIN, HIGH); SPI.endTransaction(); },
-//     [](void *, uint8_t c) { return SPI.transfer(c); }, // Execute transaction
-// };
-
-// // Construct MAC address from the unique board ID
-// #include "pico/unique_id.h"
-// static inline void genmac(unsigned char *mac) {
-//   pico_unique_board_id_t board_id;
-//   pico_get_unique_board_id(&board_id);
-//   mac[0] = 2;
-//   memcpy(&mac[1], &board_id.id[3], 5);
-// }
-// struct mg_tcpip_if mif;
-
-// void setup() {
-//   Serial.begin(115200);
-//   while (!Serial) delay(50);
-
-//   pinMode(SS_PIN, OUTPUT);
-//   digitalWrite(SS_PIN, HIGH);
-//   pinMode(LED_PIN, OUTPUT);
-//   SPI.setRX(16);
-//   SPI.setSCK(18);
-//   SPI.setTX(19);
-//   SPI.begin();
-
-//   // Set logging function to serial print
-//   mg_log_set_fn([](char ch, void *) { Serial.print(ch); }, NULL);
-//   mg_log_set(MG_LL_DEBUG);
-
-//   mongoose_init();
-
-//   // Initialise built-in TCP/IP stack with W5500 driver
-//   genmac(mif.mac);
-//   // mif.enable_dhcp_client = false;
-//   // mif.ip = MG_IPV4(192, 168, 10, 55);
-//   // mif.gw = MG_IPV4(192, 168, 10, 1);
-//   // mif.mask = MG_IPV4(255, 255, 255, 0);
-//   mif.enable_dhcp_client = true;
-//   mif.ip = MG_IPV4(0, 0, 0, 0);
-//   mif.gw = MG_IPV4(0, 0, 0, 0);
-//   mif.mask = MG_IPV4(0, 0, 0, 0);
-//   mif.driver = &mg_tcpip_driver_w5500;
-//   mif.driver_data = &spi;
-//   mg_tcpip_init(&g_mgr, &mif);
-// }
-
-// void loop() {
-//   mongoose_poll();
-// }
