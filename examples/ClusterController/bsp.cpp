0de419ac01d75c3a6661c44d26ec4e693f195340
Peter Polidoro <peter@polidoro.io>
2025-05-13 15:45:27 -0400

    Reduce home velocity

--- examples/ClusterController/bsp.cpp
+++ examples/ClusterController/bsp.cpp
@@ -91,7 +91,7 @@ const tmc51x0::HomeParameters home_parameters_real =
   .run_current = 50, // (percent)
   .hold_current = 20, // (percent)
   .target_position = -750, // (millimeters)
-  .velocity = 50, // (millimeters/s)
+  .velocity = 20, // (millimeters/s)
   .acceleration = 2, // ((millimeters/s)/s)
   .zero_wait_duration = 100 // (milliseconds)
 };
